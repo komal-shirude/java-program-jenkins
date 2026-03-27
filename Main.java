a10a25a475ec11f467a5c8440542f45434b9078c
Komal Shirude <komalshirude06@gmail.com>
2026-03-27 18:45:06 +0530

    updated Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
+
 public class Main{
 	public static void main(String args[]){
 		System.out.println("This is java program");
 		System.out.println("output from java program");
 		System.out.println("Hello");
+		System.out.println("Add one more line to test");
+
 	}
 }
