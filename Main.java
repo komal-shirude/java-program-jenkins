dcb7449895f7dccb4a20898f71448580e3e6dcbb
Komal Shirude <komalshirude06@gmail.com>
2026-03-28 15:44:31 +0530

    Added Student.java and updated Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main{
 		System.out.println("Hello");
 		System.out.println("Add one more line to test");
 	    System.out.println("One more added");
-
-
+        Student st = new Student();
+        st.sayHello();
 	}
 }
---
+++ Student.java
@@ -0,0 +1,10 @@
+Public class Student{
+
+    public Student(){
+        System.out.println("creating student obj")
+    }
+
+    public void sayHello(){
+        System.out.println("I am student")
+    }
+}
